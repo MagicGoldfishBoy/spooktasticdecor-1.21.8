c78fab54f162292f1ee150ab6f8d6bf4c684274c
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-09-04 14:00:22 -0400

    added loot table datagen

--- src/main/java/com/goldfish/spooktasticdecor/datagen/Datagen.java
+++ src/main/java/com/goldfish/spooktasticdecor/datagen/Datagen.java
@@ -1,14 +1,33 @@
 package com.goldfish.spooktasticdecor.datagen;
 
+import java.util.List;
+import java.util.Set;
+
 import net.minecraft.client.model.Model;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
 
 public class Datagen {
 
     public void gatherData(GatherDataEvent event) {
-                System.out.println("Datagen event fired!");
+        System.out.println("Datagen event fired!");
         event.createProvider(RecipeGenerator.Runner::new);
         event.createProvider(ModelDatagen::new);
+        event.createProvider((output, lookupProvider) -> new LootTableProvider(
+            output,
+            Set.of(), // Required table locations - empty set for mods
+            List.of(
+                new LootTableProvider.SubProviderEntry(
+                    LootTableDatagen::new, // Must match constructor (HolderLookup.Provider)
+                    LootContextParamSets.BLOCK
+                )
+            ),
+            lookupProvider
+        ));
     }
 }
+
---
+++ src/main/java/com/goldfish/spooktasticdecor/datagen/LootTableDatagen.java
@@ -0,0 +1,54 @@
+package com.goldfish.spooktasticdecor.datagen;
+
+import java.util.Map;
+import java.util.Set;
+import com.goldfish.spooktasticdecor.SpooktasticDecor;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+
+public class LootTableDatagen extends BlockLootSubProvider {
+
+    public LootTableDatagen(HolderLookup.Provider lookupProvider) {
+        super(
+            Set.of(), // explosionResistant items
+            FeatureFlags.REGISTRY.allFlags(), // enabledFeatures
+            new java.util.HashMap<>(), // loot table builders
+            lookupProvider
+        );
+    }
+
+    @Override
+protected Iterable<Block> getKnownBlocks() {
+    return SpooktasticDecor.BLOCKS.getEntries()
+        .stream()
+        .map(e -> (Block) e.value())
+        .filter(block -> block.asItem() != Items.AIR) // only include blocks with items
+        .toList();
+}
+
+    // @Override
+    // protected Iterable<Block> getKnownBlocks() {
+    //     return SpooktasticDecor.BLOCKS.getEntries()
+    //         .stream()
+    //         .map(e -> (Block) e.value())
+    //         .toList();
+    // }
+
+    @Override
+protected void generate() {
+    SpooktasticDecor.BLOCKS.getEntries().forEach(entry -> {
+        Block block = entry.get();
+
+        if (block.asItem() == Items.AIR) {
+        return; // Skip
+}
+
+
+        this.dropSelf(block);
+    });
+}
+}
