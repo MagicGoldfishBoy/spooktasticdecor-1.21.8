7f2bfc9e896b78659b79f437e0f271268fe566e8
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-09-23 17:08:03 -0400

    added voxels to small decor

--- src/main/java/com/goldfish/spooktasticdecor/block/SmallDecorItem.java
+++ src/main/java/com/goldfish/spooktasticdecor/block/SmallDecorItem.java
@@ -4,17 +4,18 @@ import javax.annotation.Nullable;
 
 import com.goldfish.spooktasticdecor.registry.SmallDecorItemRegistry;
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.SimpleMapCodec;
-
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class SmallDecorItem extends HorizontalDirectionalBlock {
 
@@ -30,6 +31,18 @@ public class SmallDecorItem extends HorizontalDirectionalBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, net.minecraft.world.phys.shapes.CollisionContext context) {
+        VoxelShape facing = switch (state.getValue(FACING)) {
+            case Direction.NORTH -> Shapes.box(0.3, 0, 0.6, 0.7, 0.5, 1.0);
+            case Direction.EAST -> Shapes.box(0, 0, 0.3, 0.4, 0.5, 0.7);
+            case Direction.SOUTH -> Shapes.box(0.3, 0, 0.0, 0.70, 0.5, 0.4);
+            case Direction.WEST -> Shapes.box(0.6, 0, 0.3, 1.0, 0.5, 0.7);
+            default -> Shapes.box(0.05, 0.25, 0.0, 0.945, 0.75, 0.50);
+        };
+        return facing;
     }
 
     @Override
